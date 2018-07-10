b67f9563f6940933b64a573cff0ea9eafa7a89fe
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-07-10 09:51:23 +0200

    Use commander for input parsing

--- SolcToJs.js
+++ SolcToJs.js
@@ -1,27 +1,38 @@
-const { exec } = require('child_process');
-var fs = require('fs')
-var util = require('util')
-var path = require('path')
+#!/usr/bin/env node
+'use strict';
 
-const fileName = process.argv[2];
-const objName = path.basename(fileName).split('.sol')[0];
+const { exec } = require('child_process')
+const commander = require('commander')
+const fs = require('fs')
+const util = require('util')
+const path = require('path')
 
-const child = exec('solc  --combined-json abi,bin ' + fileName, [], (error, stdout, stderr) => {
-  if (error) throw new Error(error);
-  //if (stderr) throw new Error(stderr);
-  
-  const obj = JSON.parse(stdout);
+commander
+  .version('0.1.0', '-v, --version')
+  .command('soltojs <inputFile> [outputfile]')
+  .action((paramInputFile, paramOutputFile) => {
 
-  const contractDataName = fileName + ':' + objName;
-  const contractData = {
-    abi: JSON.parse(obj.contracts[contractDataName].abi),
-    bin: '0x' + obj.contracts[contractDataName].bin
-  };
+    const absInputFile = path.resolve(paramInputFile);
 
-  var outputFile = './' + objName + '.js';
-  if (process.argv[3]) {
-    outputFile = process.argv[3];
-  }
-  
-  fs.writeFileSync(outputFile, 'module.exports = ' + util.inspect(contractData, false, 99999) , 'utf-8');
-});
+    const objName = path.basename(absInputFile).split('.sol')[0];
+
+    const child = exec('solc  --combined-json abi,bin ' + absInputFile, [], (error, stdout, stderr) => {
+      if (error) throw new Error(error);
+      //if (stderr) throw new Error(stderr);
+      
+      const obj = JSON.parse(stdout);
+      const contractDataName = absInputFile + ':' + objName;
+      const contractData = {
+        abi: JSON.parse(obj.contracts[contractDataName].abi),
+        bin: '0x' + obj.contracts[contractDataName].bin
+      };
+    
+      var outputFile = path.dirname(absInputFile) + path.sep + objName + '.js';
+      if (paramOutputFile) {
+        outputFile = paramOutputFile;
+      }
+      
+      fs.writeFileSync(outputFile, 'module.exports = ' + util.inspect(contractData, false, 99999) , 'utf-8');
+    });
+  });
+  commander.parse(process.argv);
