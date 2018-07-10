af6086b34da0d278a92db4ed4ad99054d11f8ad7
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-07-10 11:07:33 +0200

    Moved compilation to separate function

--- ethtools.js
+++ ethtools.js
@@ -9,30 +9,50 @@ const path = require('path')
 
 commander
   .version('0.1.0', '-v, --version')
+
   .command('soltojs <inputFile> [outputfile]')
   .action((paramInputFile, paramOutputFile) => {
+    var absInputFile = path.resolve(paramInputFile);
 
-    const absInputFile = path.resolve(paramInputFile);
-
-    const objName = path.basename(absInputFile).split('.sol')[0];
-
-    const child = exec('solc  --combined-json abi,bin ' + absInputFile, [], (error, stdout, stderr) => {
-      if (error) throw new Error(error);
-      //if (stderr) throw new Error(stderr);
-      
-      const obj = JSON.parse(stdout);
-      const contractDataName = absInputFile + ':' + objName;
-      const contractData = {
-        abi: JSON.parse(obj.contracts[contractDataName].abi),
-        bin: '0x' + obj.contracts[contractDataName].bin
-      };
-    
-      var outputFile = path.dirname(absInputFile) + path.sep + objName + '.js';
+    solc(absInputFile).then((contractData) => {
+      var outputFile = absInputFile.replace(/.sol$/, '.js')
       if (paramOutputFile) {
         outputFile = paramOutputFile;
       }
-      
       fs.writeFileSync(outputFile, 'module.exports = ' + util.inspect(contractData, false, 99999) , 'utf-8');
     });
   });
-  commander.parse(process.argv);
+
+commander.parse(process.argv);
+
+function solc(inputFile, outputTypes = ['abi', 'bin'], pathToCompiler = 'solc') {
+  return new Promise((resolve, reject) => {
+    exec(pathToCompiler + ' --combined-json ' + outputTypes.join(',') +' ' + inputFile,[], (error, stdout, stderr) => {
+      if (error) reject(error)
+      if (stderr) reject(stderr);
+
+      const compilerOutput = JSON.parse(stdout)
+      const targetObjectName = path.basename(inputFile).split('.sol')[0]
+      const contractDataID = inputFile + ':' + targetObjectName
+
+      var contractData = {}
+      outputTypes.forEach(outputType => {
+        switch(outputType) {
+          case 'abi':
+            contractData.abi = null
+            if (compilerOutput.contracts[contractDataID].abi) {
+              contractData.abi = JSON.parse(compilerOutput.contracts[contractDataID].abi)
+            }
+            break
+          case 'bin':
+            contractData.bin = null
+            if (compilerOutput.contracts[contractDataID].bin) {
+              contractData.bin = '0x' + compilerOutput.contracts[contractDataID].bin
+            }
+        }
+      });
+      
+      resolve(contractData)
+    })
+  })
+}
